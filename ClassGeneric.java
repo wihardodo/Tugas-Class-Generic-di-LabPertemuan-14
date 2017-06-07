58957350391698d6eeaa009b51a80539aa760f9f
wihardodo <wihardodo@gmail.com>
2017-06-08 05:37:47 +0700

    Add files via upload

---
+++ ClassGeneric.java
@@ -0,0 +1,58 @@
+package classgeneric;
+
+public class ClassGeneric {
+
+public static void main(String[] args) {
+public class Handphone Asus<T> {
+    private T menyala;
+    public Handphone Asus (T menyala){
+        this.menyala=menyala;
+    }
+    public void setmenyala(T menyala){
+        this.menyala=menyala;
+    }
+    public T getmenyala(){
+        return menyala;
+    }
+    public void getType(){
+        system.out.println("Tipe Awalnya"+menyala.getClass().getName());
+    }
+	public class Method{
+		public static void main(string[] args){
+		Method.method=new Method();
+		Method.methodmenyalakan();
+		System.out.println(method.kembalikanNilai();
+		}
+	private void methodMenyalakan(){
+        System.out.println("Tekan tombol on off");
+        }
+	private int matikanHandphone(){
+        return 1;
+        }
+}
+}
+public class Handphone Xiaomi<T> {
+    private T menelfon;
+    public Handphone Xiaomi (T menelefon){
+        this.menelfon=menelfon;
+    }
+    public void setmenelfon(T meneflon){
+        this.menelfon=menelfon;
+    }
+    public T getmenelfon(){
+        return menelfon;
+    }
+    public void getType(){
+        system.out.println("Tipe Awalnya"+menelfon.getClass().getName());
+    }
+	Method.method=new Method();
+		Method.methodmenelfon();
+		System.out.println(method.kembalikanNilai();
+		}
+	private void methodmenelfo(){
+        System.out.println("Tekan tombol Cancel");
+        }
+	private int batalkanpanggilan(){
+        return 1;
+}
+}
---
+++ methodgenericdanobjek.java
@@ -0,0 +1,51 @@
+package classgeneric;
+public class methodgenericdanobjek {
+  Class Handphone <T> {
+	private T noImei;
+	public Handphone (T noImei){
+		this.noImei=noImei;
+		}
+	public void SetnoIme(T noImei){
+		this.noImei=noImei;
+		}
+	public T getnoImei(){
+		return noImei;
+		}
+		public void getType(){
+			System.out.println("Tipe Awalnya" + noOmei.getClass().getName());
+		}
+	}
+	
+	public Class Generic 
+		public static void main (string[] args){
+		Handphone<String> H=new Handphone<String>(Samsung);
+		int y=H.getnoImei();
+		System.out.println("Type string :" + y");
+		System.out.println(". . . .");
+
+		public static void main(string[] args){
+		Method.method=new Method();
+		Method.methodmenyalakan();
+		System.out.println(method.kembalikanNilai();
+		}
+        private void methodMenyalakan(){
+        System.out.println("Tekan tombol on off");
+                private int matikanHandphone(){
+                         return 1;
+}
+		Handphone<String>H2=new Handphone<String>("Sony Experia");
+		String z=h.getnoIme();
+		System.out.println("Type string :" + z");
+		
+}               Method.method=new Method();
+		Method.methodmenelfon();
+		System.out.println(method.kembalikanNilai();
+		}
+	private void methodmenelfo(){
+        System.out.println("Tekan tombol Cancel");
+        }
+	private int batalkanpanggilan(){
+                        return 1;
+
+	}  
+}
